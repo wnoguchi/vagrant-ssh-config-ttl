950feb54654c6476451559db71f785f3c84e14cc
Wataru Noguchi <wnoguchi.0727@gmail.com>
2015-08-15 13:18:50 +0900

    Initial commit.

---
+++ lib/vagrant/ssh/config/ttl.rb
@@ -0,0 +1,11 @@
+require "vagrant/ssh/config/ttl/version"
+
+module Vagrant
+  module Ssh
+    module Config
+      module Ttl
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/vagrant/ssh/config/ttl/version.rb
@@ -0,0 +1,9 @@
+module Vagrant
+  module Ssh
+    module Config
+      module Ttl
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
